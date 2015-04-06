fd361a7506a54421694836741058bb7fb399c0fe
Mike <mike@odie.(none)>
2015-04-06 07:44:49 +0200

    added command line arguments to rename_alignments.py

--- rename_alignments.py
+++ rename_alignments.py
@@ -4,31 +4,44 @@ import pdb
 from Bio import AlignIO
 from sys import argv
 from Bio.Alphabet import ProteinAlphabet
-in_file = 'species_list.csv'
 
-names = {}
-for row in csv.reader(open(in_file), delimiter=','):
-    if 'letter code' in  row[0]:
-        continue
+import argparse
 
-    sp = row[1].replace(' ', '_')
-    cls = row[2]
-    names[row[0]] = sp
+def parse_cli():
+    parser = argparse.ArgumentParser(description="Rename alignments")
 
-work_dir = 'align-SH'
+    parser.add_argument('-i', type=str, dest='work_dir', default='alignments',
+                        help='directory containing aligned sequences')
 
+    parser.add_argument('-s', type=str, dest='in_file',
+                        help='text file containing the file names and three letter codes')
 
-for file in os.listdir(work_dir):
-    if file.endswith('aligned.fasta'):
-        #outseqs = []
-        alpha = ProteinAlphabet()
-        alignment = AlignIO.read(work_dir+'/'+file, 'fasta', alphabet=alpha)
-        for seq in alignment:
-            seq.id = names[seq.id]
-            #outseqs.append(seq)
+    opts = parser.parse_args()
+
+    return opts
+
+if __name__ == '__main__':
+    cli_args = parse_cli()
+    names = {}
+    for row in csv.reader(open(cli_args.in_file), delimiter=','):
+        if 'letter code' in  row[0]:
+            continue
+
+        sp = row[1].replace(' ', '_')
+        cls = row[2]
+        names[row[0]] = sp
+
+    for file in os.listdir(cli_args.work_dir):
+        if file.endswith('aligned.fasta'):
+            #outseqs = []
+            alpha = ProteinAlphabet()
+            alignment = AlignIO.read(cli_args.work_dir+'/'+file, 'fasta', alphabet=alpha)
+            for seq in alignment:
+                seq.id = names[seq.id]
+                #outseqs.append(seq)
+                #pdb.set_trace()
+            outfile = cli_args.work_dir + '/' + file + '.renamed.nex'
             #pdb.set_trace()
-        outfile = work_dir + '/' + file + '.renamed.nex'
-        #pdb.set_trace()
-        outfile_h = open(outfile, 'w')
-        AlignIO.write([alignment], outfile_h, 'nexus')
-        outfile_h.close()
+            outfile_h = open(outfile, 'w')
+            AlignIO.write([alignment], outfile_h, 'nexus')
+            outfile_h.close()
